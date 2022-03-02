7f6ca5d22cf568a12e522d0198606eea3fc4520f
pratikmdhr71 <pratikmndh@yahoo.com>
2022-03-01 23:54:46 -0500

    chore: setup database and update sequelize connection params

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:5432/messenger`,
+  {
+    logging: false,
+  },
+);
 
 module.exports = db;
